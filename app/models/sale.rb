c6ee6a511f49c890a47f6f4cf745e0213f59364b
Pavel Gabriel <alovak@gmail.com>
2011-07-13 23:50:34 +0300

    fixed sql query for MySQL (use = instead == an in sqlite)

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -3,5 +3,5 @@ class Sale < ActiveRecord::Base
   belongs_to :vendor
 
 
-  scope :for_vendor, lambda { |name| where("vendor_id == ?", Vendor.find_by_name(name)) }
+  scope :for_vendor, lambda { |name| where("vendor_id = ?", Vendor.find_by_name(name)) }
 end
