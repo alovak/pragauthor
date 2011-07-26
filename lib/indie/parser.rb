2602632cc6aa581ee11e16bc5067b18401d0ea35
Pavel Gabriel <alovak@gmail.com>
2011-07-26 21:37:26 +0300

    detect and create parser by provided file name

--- lib/indie/parser.rb
+++ lib/indie/parser.rb
@@ -1,5 +1,21 @@
 module Indie
   module Parser
+    class << self
+      def factory(file_path) 
+        parser_class = detect_parser_by_file(file_path)
+        parser_class.new(file_path)
+      end
+
+      private
+
+      def detect_parser_by_file(file_path)
+        case file_path
+        when /bnsales.*xls$/i then Indie::Parser::BarnesNoble
+        when /smashwords_salesreport.*xls$/i then Indie::Parser::Smashwords
+        end
+      end
+    end
+
     class Base
     end
   end
---
+++ spec/lib/indie/parser_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Indie::Parser do
+  describe "#factory" do
+    it "should return BarnesNoble parser for Barnes&Noble file" do
+      parser = Indie::Parser.factory('./path/BNsales_May2011.xls')
+      parser.should be_an_instance_of Indie::Parser::BarnesNoble
+    end
+
+    it "should return Smashwords parser for Smashwords file" do
+      parser = Indie::Parser.factory('./path/SmashWords_salesReport-2011-06-08.xls')
+      parser.should be_an_instance_of Indie::Parser::Smashwords
+    end
+  end
+end
