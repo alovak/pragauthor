05609526a3988086fb298a026db39c73b224117d
Pavel Gabriel <alovak@gmail.com>
2011-08-03 13:31:07 +0300

    amazon parser was implemented

--- lib/indie/parser/amazon.rb
+++ lib/indie/parser/amazon.rb
@@ -1,8 +1,47 @@
 module Indie
   module Parser
     class Amazon < Base
-      def process
 
+      TITLE         = 0
+      UNIT_NET_SALES = 4
+      BOOKS_OFFSET   = 4
+      SALE_DATE_OFFSET = 2
+
+      def initialize(file_path)
+        @file_path = file_path
+      end
+
+      def process
+        @book = Spreadsheet.open(@file_path)
+        sheet = @book.worksheet 0
+        sheet.each do |row|
+          process_books_for_store(sheet, row.idx) if row[0] == 'Title'
+        end
+      end
+
+      private
+
+      def process_books_for_store(sheet, header_row_id)
+        return if sheet.row(header_row_id + BOOKS_OFFSET)[0] == 'There were no sales during this period.'
+
+        sale_date = convert_date(sheet.row(header_row_id + SALE_DATE_OFFSET)[0])
+
+        sheet.each(header_row_id + BOOKS_OFFSET) do |row|
+          return if row[TITLE] =~ /Total Royalty for Sales/
+          book = Book.find_or_create_by_title(row[TITLE])
+          book.sales.create(:units => row[UNIT_NET_SALES], :vendor => vendor, :date_of_sale => sale_date)
+        end
+      end
+
+      def vendor
+        @vendor ||= Vendor.find_by_name('Amazon')
+      end
+
+      def convert_date(date)
+        # match with:
+        #   Sales report for the period 01-Apr-2011 to 30-Apr-2011
+        matches = date.match(/period (\d{2}-\w{3}-\d{4}) to/)
+        DateTime.strptime(matches[1], '%d-%b-%Y')
       end
     end
   end
--- spec/lib/indie/parser/amazon_spec.rb
+++ spec/lib/indie/parser/amazon_spec.rb
@@ -1,4 +1,60 @@
 require 'spec_helper'
 
 describe Indie::Parser::Amazon do
+  let(:file) do
+    "#{Rails.root}/features/support/files/kdp-report-04-2011.xls"
+  end
+
+  let(:parser) do
+    described_class.new(file)
+  end
+
+  context "when there are no any books in the app" do
+    before do
+      Book.delete_all
+    end
+
+    context "and file was processed" do
+      before do
+        parser.process
+      end
+
+      it "should create books" do
+        Book.count.should == 3
+      end
+
+      it "should create books with corresponding titles" do
+        ['The First Book', 'The Second Book', 'The Third Book'].each do |title|
+          book = Book.find_by_title(title)
+          book.should_not be_nil
+        end
+      end
+
+      it "should create sales" do
+        Sale.count.should == 3
+      end
+
+      it "should create sales with correct dates" do
+        Sale.where(:date_of_sale => DateTime.parse('1 Apr 2011')).all.should have(3).sales
+      end
+
+      it "should create sales with units for the books" do
+        { 'The First Book'  => 190, 
+          'The Second Book' => 13,
+          'The Third Book'  => 5
+        }.each do |title, units|
+          book = Book.find_by_title(title)
+          book.sales.sum(:units).should == units
+        end
+      end
+
+      it "should assign 'Amazon' as vendor to all sales" do
+        bn = Vendor.find_by_name('Amazon')
+
+        Sale.all.each do |sale|
+          sale.vendor.should == bn
+        end
+      end
+    end
+  end
 end
