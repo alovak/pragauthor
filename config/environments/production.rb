33a007e2eaf729e2850e9fef7ac21fe3822539dd
Pavel Gabriel <alovak@gmail.com>
2011-08-16 16:33:35 +0300

    set mailer host for production environmetn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,4 +51,6 @@ Indie::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'indie.com' }
 end
