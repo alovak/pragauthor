98d615ff4f98566b180581e08134be5ef726c5de
Pavel Gabriel <alovak@gmail.com>
2011-09-03 20:07:24 +0300

    shared examples were created (extracted from Smashwords)

--- lib/indie/parser/smashwords.rb
+++ lib/indie/parser/smashwords.rb
@@ -4,6 +4,8 @@ module Indie
   module Parser
     class Smashwords < Base
 
+      VENDOR_NAME     = 'Smashwords'
+
       SKIP_ROWS       = 1
       SALE            = 0
       TITLE           = 5
@@ -22,11 +24,6 @@ module Indie
       end
 
       private
-
-
-      def vendor
-        @vendor ||= Vendor.find_by_name('Smashwords')
-      end
 
       def convert_date(date)
         DateTime.strptime(date, '%Y-%m-%d')
