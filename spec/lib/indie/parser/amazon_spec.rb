d0487f8ff705a5da56ec93e18fb4c55381d47e30
Pavel Gabriel <alovak@gmail.com>
2011-09-04 12:14:42 +0300

    spec for amazon parser was updated

--- spec/lib/indie/parser/amazon_spec.rb
+++ spec/lib/indie/parser/amazon_spec.rb
@@ -1,64 +1,29 @@
 require 'spec_helper'
+require 'lib/indie/parser/shared_examples_for_parser'
 
 describe Indie::Parser::Amazon do
   let(:file) do
     "#{Rails.root}/features/support/files/kdp-report-04-2011.xls"
   end
 
-  let(:user) { Factory(:user) }
+  expectations = {
+    :titles => ['The First Book', 'The Second Book', 'The Third Book'],
+    :books  => 3,
+    :sales  => 3,
+    :book_units => { 
+      'The First Book'  => 190, 
+      'The Second Book' => 13,
+      'The Third Book'  => 5
+    }
+  }
 
-  let(:parser) do
-    described_class.new(file, user)
-  end
+  it_should_behave_like "a parser", expectations
 
-  context "when there are no any books in the app" do
-    before do
-      Book.delete_all
-    end
+  include_context "parser stuff"
 
-    context "and file was processed" do
-      before do
-        parser.process
-      end
+  it "should create sales with correct dates" do
+    parser.process
 
-      it "should create books" do
-        Book.count.should == 3
-      end
-
-      it "should create books with corresponding titles" do
-        ['The First Book', 'The Second Book', 'The Third Book'].each do |title|
-          book = Book.find_by_title(title)
-          book.should_not be_nil
-        end
-      end
-
-      it "should create sales" do
-        Sale.count.should == 3
-      end
-
-      it "should create sales with correct dates" do
-        Sale.where(:date_of_sale => DateTime.parse('1 Apr 2011')).all.should have(3).sales
-      end
-
-      it "should create sales with units for the books" do
-        { 'The First Book'  => 190, 
-          'The Second Book' => 13,
-          'The Third Book'  => 5
-        }.each do |title, units|
-          book = Book.find_by_title(title)
-          book.sales.sum(:units).should == units
-        end
-      end
-
-      it "should assign 'Amazon' as vendor to all sales" do
-        vendor = Vendor.find_by_name('Amazon')
-
-        vendor.should_not be_nil
-
-        Sale.all.each do |sale|
-          sale.vendor.should == vendor
-        end
-      end
-    end
+    Sale.where(:date_of_sale => DateTime.parse('1 Apr 2011')).all.should have(3).sales
   end
 end
