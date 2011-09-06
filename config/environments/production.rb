99855c8c5c5705c0b60b84cca1d10b4d9605dcaf
Pavel Gabriel <alovak@gmail.com>
2011-09-06 18:09:23 +0300

    production evn updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,12 @@ Indie::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
+
   # Specify the default JavaScript compressor
   config.assets.js_compressor  = :uglifier
 
