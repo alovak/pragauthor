36bd7acf24321900265f3462f9b32276f2f6f6bc
Pavel Gabriel <alovak@gmail.com>
2011-09-21 14:45:17 +0300

    added colors for selected month

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,31 @@ module ApplicationHelper
     controller_name.gsub('/', '__')
   end
 
+  def month_color(month)
+    colors = {
+      'Jan' => %w(d67534 d67534), 
+      'Feb' => %w(bb2b24 bb2b24), 
+      'Mar' => %w(118793 118793), 
+      'Apr' => %w(92a489 92a489), 
+      'May' => %w(d1ad58 d1ad58), 
+      'Jun' => %w(714c94 714c94), 
+      'Jul' => %w(d67534 d67534), 
+      'Aug' => %w(bb2b24 bb2b24), 
+      'Sep' => %w(118793 118793), 
+      'Oct' => %w(92a489 92a489), 
+      'Nov' => %w(d1ad58 d1ad58), 
+      'Dec' => %w(714c94 714c94), 
+    }
+    month_name =  case month
+                  when String then month
+                  when Date then month.strftime('%b')
+                  end
+
+
+    dark, light = colors[month_name]
+    { :dark => dark, :light => light }
+  end
+
   def barchart(width, months )
     render :partial => 'line', :collection => bardata(width, months), :as => :record
   end
@@ -15,7 +40,7 @@ module ApplicationHelper
       months.each_with_index do |month, index| 
         bars << { 
           :value => month.units,
-          :color => "##{colors[index]}", 
+          :color => "##{month_color(month.name)[:dark]}", 
           :width => month.units*width/max + 2,
           :label => month.name
         }
