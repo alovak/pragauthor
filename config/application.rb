f8718e93f8b13460490e48ca35be1ed2685c3e70
Pavel Gabriel <alovak@gmail.com>
2011-09-28 15:14:47 +0300

    flash messages were added to welcome layout. use devise lay. for edit account

--- config/application.rb
+++ config/application.rb
@@ -48,7 +48,7 @@ module Indie
 
     config.to_prepare do
       Devise::SessionsController.layout "devise"
-      Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "application" : "devise" }
+      Devise::RegistrationsController.layout "devise"
       Devise::ConfirmationsController.layout "devise"
       Devise::UnlocksController.layout "devise"            
       Devise::PasswordsController.layout "devise"        
