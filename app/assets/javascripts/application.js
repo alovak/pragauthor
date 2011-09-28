2d9cf75c63da4296c29ef4e2d05a09a71d40c44a
Pavel Gabriel <alovak@gmail.com>
2011-09-28 10:58:14 +0300

    changed chart behaviour (one color + selected line)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,15 @@ jQuery(document).ready(function($) {
   $.facebox.settings.closeImage = '/assets/facebox/closelabel.png'
   $.facebox.settings.loadingImage = '/assets/facebox/loading.gif'
   $('a[rel*=facebox]').facebox()
+
+  $("div.column").mouseenter(function(){
+    $(this).addClass('selected')
+    month = $(this).siblings('.label').text();
+    $(this).parents('.barchart').siblings(".month" + "." + month).show();
+    
+  }).mouseleave(function(){
+    $(this).removeClass('selected')
+    month = $(this).siblings('.label').text();
+    $(this).parents('.barchart').siblings(".month" + "." + month).hide();
+  });
 })
