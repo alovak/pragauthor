c025c831a82a39a760442e1d6c1ffa2e9b17ecd1
Pavel Gabriel <alovak@gmail.com>
2011-12-30 22:53:52 +0200

    add vendors tab

--- lib/indie/chart.rb
+++ lib/indie/chart.rb
@@ -1,5 +1,168 @@
 module Indie
   module Chart
+    class VendorSales
+      def initialize(sales, options = {})
+        @sales = sales
+        @top_books = options[:top] || 5
+        @period = options[:period] || 6
+        @show_trend = options[:show_trend]
+      end
+
+      def data
+        @data ||= { 
+          cols: cols,
+          rows: rows
+        }
+      end
+
+      def show_trend?
+        @show_trend == true
+      end
+
+      private
+
+      def items
+        @items ||= Vendor.all
+      end
+
+      def cols
+        [].tap do |cols|
+          cols << { label: 'Month', type: 'string' }
+
+          items.inject(cols) { |acc, i| acc << {label: i.name, type: 'number'} }
+
+          if show_trend?
+            cols << { label: 'Average', type: 'number' }
+            cols << { label: 'Totals',  type: 'number' }
+          end
+        end
+      end
+
+      def rows
+        [].tap do |rows|
+          months.each do |month|
+            row = { c: [] }
+
+            row[:c] << { v: month.strftime('%b') }
+
+            sum = 0
+
+            items.each do |item|
+              raw_key = [month.year, month.month, item.id]
+              units = raw_data[raw_key] || 0
+
+              sum = sum + units
+
+              row[:c] << { v: units }
+            end
+
+            if show_trend? && items.any?
+              row[:c] << { v: sum/items.count }
+              row[:c] << { v: sum }
+            end
+
+            rows << row
+          end
+        end
+      end
+
+      def months
+        (@period-1).downto(0).collect { |m| Date.new(m.month.ago.year, m.month.ago.month) }
+      end
+
+      def raw_data
+        @sales
+          .where("date_of_sale > ?", @period.month.ago.end_of_month)
+          .group("year(date_of_sale)")
+          .group("month(date_of_sale)")
+          .group(:vendor_id)
+          .sum(:units)
+      end
+    end
+
+    class VendorMoney
+      def initialize(sales, options = {})
+        @sales = sales
+        @top_books = options[:top] || 5
+        @period = options[:period] || 6
+        @show_trend = options[:show_trend]
+        @currency   = options[:currency] || 'USD'
+      end
+
+      def data
+        @data ||= { 
+          cols: cols,
+          rows: rows
+        }
+      end
+
+      def show_trend?
+        @show_trend == true
+      end
+
+      private
+
+      def items
+        @items ||= Vendor.all
+      end
+
+      def cols
+        [].tap do |cols|
+          cols << { label: 'Month', type: 'string' }
+
+          items.inject(cols) { |acc, i| acc << {label: i.name, type: 'number'} }
+
+          if show_trend?
+            cols << { label: 'Average', type: 'number' }
+            cols << { label: 'Totals',  type: 'number' }
+          end
+        end
+      end
+
+      def rows
+        [].tap do |rows|
+          months.each do |month|
+            row = { c: [] }
+
+            row[:c] << { v: month.strftime('%b') }
+
+            sum = 0
+
+            items.each do |item|
+              #key for raw_data
+              raw_key = [month.year, month.month, item.id]
+
+              units = raw_data[raw_key] || 0
+
+              sum = sum + units
+
+              row[:c] << { v: ::Money.new(units, @currency).dollars, f: ::Money.new(units, @currency).format }
+            end
+
+            if show_trend? && items.any?
+              row[:c] << { v: ::Money.new(sum/items.count, @currency).dollars, f: ::Money.new(sum/items.count, @currency).format }
+              row[:c] << { v: ::Money.new(sum, @currency).dollars, f: ::Money.new(sum, @currency).format }
+            end
+
+            rows << row
+          end
+        end
+      end
+
+      def months
+        (@period-1).downto(0).collect { |m| Date.new(m.month.ago.year, m.month.ago.month) }
+      end
+
+      def raw_data
+        @sales
+          .where("date_of_sale > ? and currency = ?", @period.month.ago.end_of_month, @currency)
+          .group("year(date_of_sale)")
+          .group("month(date_of_sale)")
+          .group(:vendor_id)
+          .sum(:amount)
+      end
+    end
+
     class Money
       def initialize(sales, options = {})
         @sales = sales
