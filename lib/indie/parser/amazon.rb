07170d9df7248db85f4c90e6a566495e542640b0
Pavel Gabriel <alovak@gmail.com>
2012-01-29 18:42:28 +0200

    add amazon v2 parser (not happy with the solution)

--- lib/indie/parser/amazon.rb
+++ lib/indie/parser/amazon.rb
@@ -4,17 +4,84 @@ module Indie
       VENDOR_NAME       = 'Amazon'
 
       def process
-        parser = AmazonParserV1.new(self)
+        parser = v2? ? AmazonParserV2.new(self) : AmazonParserV1.new(self)
         parser.process
       end
 
       private
+
+      def v2?
+        sheet.row(0)[2] =~ /Transaction Type/i
+      end
 
       def sheet
         @sheet ||= begin
                      book  = Spreadsheet.open(file_path)
                      sheet = book.worksheet 0
                    end
+      end
+    end
+
+    class AmazonParserV2
+      TITLE             = 0
+      UNIT_NET_SALES    = 5
+      CURRENCY          = 10
+      REVENUE           = 11
+
+      BOOKS_OFFSET      = 4
+      CURRENCY_OFFSET   = 1
+      SALE_DATE_OFFSET  = 2
+
+      delegate :sheet, :find_or_create_book, :create_sale, :to => :@parser
+
+      def initialize(parser)
+        @parser = parser
+      end
+
+      def process
+        sheet.each do |row|
+          process_books_for_store(row.idx) if beginning_of_the_store?(row) && store_has_sales?(row)
+        end
+      end
+
+      private
+
+      def process_books_for_store(header_row_id)
+        sale_date = convert_date(sheet.row(header_row_id + SALE_DATE_OFFSET)[0])
+        currency = get_currency(sheet.row(header_row_id + CURRENCY_OFFSET)[CURRENCY])
+
+        sheet.each(header_row_id + BOOKS_OFFSET) do |row|
+          return if end_of_store?(row)
+
+          book = find_or_create_book(row[TITLE])
+          create_sale(book, :units => row[UNIT_NET_SALES], 
+                            :date_of_sale => sale_date, 
+                            :amount => row[REVENUE].to_money.cents,
+                            :currency => currency)
+        end
+      end
+
+      def beginning_of_the_store?(row)
+        row[TITLE] =~ /Title/i
+      end
+
+      def end_of_store?(row)
+        row[TITLE] =~ /Total Royalty for Sales/i
+      end
+
+      def store_has_sales?(row)
+        sheet.row(row.idx + BOOKS_OFFSET)[0] !~ /There were no sales during this period/i
+      end
+
+      def get_currency(currency)
+        currency.gsub(/\(|\)| /, '')
+      end
+
+      def convert_date(date)
+        # match with:
+        #   Sales report for the period 01-Apr-2011 to 30-Apr-2011
+        matches = date.match(/period (\d{2}-\w{3}-\d{4}) to/)
+        DateTime.strptime(matches[1], '%d-%b-%Y')
       end
     end
 
---
+++ spec/lib/indie/parser/amazon_v2_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+require 'lib/indie/parser/shared_examples_for_parser'
+
+describe Indie::Parser::Amazon do
+  let(:file) do
+    "#{Rails.root}/features/support/files/v2_kdp-report-04-2011.xls"
+  end
+
+  expectations = {
+    :titles => ['The First Book', 'The Second Book', 'The Third Book'],
+    :books  => 3,
+    :sales  => 3,
+    :book_units => { 
+      'The First Book'  => 190, 
+      'The Second Book' => 13,
+      'The Third Book'  => 5
+    },
+    :book_money => { 
+      'The First Book'  => Money.us_dollar(66_58), 
+      'The Second Book' => Money.us_dollar(6_76),
+      'The Third Book'  => Money.us_dollar(1_75)
+    }
+  }
+
+  it_should_behave_like "a parser", expectations
+
+  include_context "parser stuff"
+
+  it "should create sales with correct dates" do
+    parser.process
+
+    Sale.where(:date_of_sale => DateTime.parse('1 Apr 2011')).all.should have(3).sales
+  end
+
+  it "should create sales with correct money" do
+
+  end
+end
