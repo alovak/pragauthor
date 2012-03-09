decd770b48d42c9c0cbd737902242bf87ab55b14
Pavel Gabriel <alovak@gmail.com>
2012-03-09 23:46:03 +0200

    move uploads to private folder. share uploads btwn releases

--- app/uploaders/report_uploader.rb
+++ app/uploaders/report_uploader.rb
@@ -13,7 +13,7 @@ class ReportUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{Rails.root}/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   process :import
