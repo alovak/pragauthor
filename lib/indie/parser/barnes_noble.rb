d3cf9c028bb737b8b5462ba40c1eba934e8a349e
Pavel Gabriel <alovak@gmail.com>
2012-03-09 23:06:50 +0200

    make b&n parser more flexible

--- lib/indie/parser/barnes_noble.rb
+++ lib/indie/parser/barnes_noble.rb
@@ -3,33 +3,39 @@ module Indie
     class BarnesNoble < Base
 
       VENDOR_NAME       = 'Barnes&Noble'
-
       SKIP_ROWS       = 1
-      DATE_OF_SALE    = 0
-      TITLE           = 3
-      UNIT_NET_SALES  = 8
-
-      REVENUE         = 9
 
       CURRENCY_CODE   = "USD"
+
+      attr_reader :title_column, :unit_net_sales_column, :date_of_sale_column, :revenue_column
 
       def process
         @book = Spreadsheet.open(@file_path)
 
+        set_columns
+
         sheet = @book.worksheet 0
 
         sheet.each(SKIP_ROWS) do |row|
-          unless row[TITLE].blank?
-            book = find_or_create_book(row[TITLE])
-            create_sale(book, :units => row[UNIT_NET_SALES], 
-                              :date_of_sale => convert_date(row[DATE_OF_SALE]), 
-                              :amount => row[REVENUE].to_money.cents, 
+          unless row[title_column].blank?
+            book = find_or_create_book(row[title_column])
+            create_sale(book, :units => row[unit_net_sales_column], 
+                              :date_of_sale => convert_date(row[date_of_sale_column]), 
+                              :amount => row[revenue_column].to_money.cents, 
                               :currency => CURRENCY_CODE)
           end
         end
       end
 
       private
+
+      def set_columns
+        header = @book.worksheet(0).row(0)
+        @title_column = header.find_index "Title"
+        @unit_net_sales_column = header.find_index "Net Unit Sales"
+        @date_of_sale_column = header.find_index "Date Of Sale"
+        @revenue_column = header.find_index "Total Publisher's Revenue"
+      end
 
       def convert_date(date)
         DateTime.strptime(date, '%m/%d/%Y')
